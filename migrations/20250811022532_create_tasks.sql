51ac07cb796677bbc75a70afc9d7f1f521913b32
hossanachukwunyere <hchukwunyere@digicoreltd.com>
2025-08-16 04:45:52 +0100

    feat(task): created_at & updated_at

--- migrations/20250811022532_create_tasks.sql
+++ migrations/20250811022532_create_tasks.sql
@@ -3,5 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE tasks (
        id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
        title TEXT NOT NULL,
-       status TEXT NOT NULL DEFAULT 'NotStarted' CHECK (status IN ('NotStarted', ' InProgress', 'Completed'))
+       status TEXT NOT NULL DEFAULT 'NotStarted' CHECK (status IN ('NotStarted', ' InProgress', 'Completed')),
+       created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+       updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
--- src/data/task_model.rs
+++ src/data/task_model.rs
@@ -1,4 +1,5 @@
 use sqlx::{FromRow, Type};
+use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
@@ -6,6 +7,25 @@ pub struct Task {
     pub id: Option<String>,
     pub title: String,
     pub status: Option<TaskStatus>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
+
+impl Task {
+    pub fn new(title: String, status: String) -> Self {
+        let now = Utc::now();
+        Self {
+            id: None,
+            title,
+            status: Option::from(TaskStatus::NotStarted),
+            created_at: Option::from(now),
+            updated_at: Option::from(now),
+        }
+    }
+
+    pub fn touch(&mut self) {
+        self.updated_at = Option::from(Utc::now());
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Type)]
