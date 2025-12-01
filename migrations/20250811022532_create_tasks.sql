2faa3138be02a35ecc28fd1e5040072c2fd91999
Hossana Chukwunyere <hchukwunyere@digicoreltd.com>
2025-12-01 19:13:45 +0100

    feat(main): auto migration, docs

--- migrations/20250811022532_create_tasks.sql
+++ migrations/20250811022532_create_tasks.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE tasks (
+CREATE TABLE IF NOT EXISTS tasks (
        id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
        title TEXT NOT NULL,
        status TEXT NOT NULL DEFAULT 'NotStarted' CHECK (status IN ('NotStarted', 'InProgress', 'Completed')),
